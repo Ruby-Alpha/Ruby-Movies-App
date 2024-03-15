dd89f60583204c549dcc8bfc050e932936bf05d2
Dammy Ola <159774611+Rofiatmuritala@users.noreply.github.com>
2024-03-15 10:09:32 +0000

    Working on the details page

--- src/Pages/Moviedetails.jsx
+++ src/Pages/Moviedetails.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable array-callback-return */
 import React, { useEffect, useState } from "react";
-import Navbar from "../Component/Navbar";
 import { useParams } from "react-router-dom";
 
 export default function MovieDetailsPage() {
@@ -12,12 +11,14 @@ export default function MovieDetailsPage() {
   const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${params.id}`;
   const reviewsUrl = `https://api.themoviedb.org/3/movie/${params.id}/reviews`;
   const websiteurl = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
+  const backdrop = "https://image.tmdb.org/t/p/original";
 
   async function fetchMovieDetails() {
     try {
       const response = await fetch(movieDetailsUrl, {
         headers: {
-          Authorization: process.env.MOVIE_SEARCH_APP_API,
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YjBmZTVmODFhNjRhNjg3YzQ5ZWQ0MDlkNjc2Mjg0OCIsInN1YiI6IjY1ZWVlNzRhMmIxMTNkMDE3ZGY5NjU0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T9GmVfISLcA2ZAkPPPHXS8eXzFzeAeEYSkGZ7C2zzX4",
           accept: "application/json",
         },
       });
@@ -33,7 +34,8 @@ export default function MovieDetailsPage() {
     try {
       const response = await fetch(reviewsUrl, {
         headers: {
-          Authorization: process.env.MOVIE_SEARCH_APP_API,
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YjBmZTVmODFhNjRhNjg3YzQ5ZWQ0MDlkNjc2Mjg0OCIsInN1YiI6IjY1ZWVlNzRhMmIxMTNkMDE3ZGY5NjU0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T9GmVfISLcA2ZAkPPPHXS8eXzFzeAeEYSkGZ7C2zzX4",
           accept: "application/json",
         },
       });
@@ -51,23 +53,34 @@ export default function MovieDetailsPage() {
   }, [params.id]);
 
   return (
-    <>
-      <Navbar />
-      <div className="movie-details-div flex">
-        <div className="movie-poster-div">
-          <img src={`${websiteurl}${movie.poster_path}`} alt="" />
-        </div>
-        <div className="movie-info-div">
-          <h1>{movie.title}</h1>
-          <p>{movie.release_date}</p>
-          {movie.genres.map((genre) => {
+    <div className="relative h-[600px]">
+      <img
+        src={`${backdrop}${movie.backdrop_path}`}
+        alt=""
+        className="h-full w-full bg-black opacity-20"
+      />
+      <div className="absolute top-0 left-0 right-0 mx-auto w-[90%] py-10">
+        <div className="grid grid-cols-10 gap-10">
+          <div className="col-span-2">
+            <img src={`${websiteurl}${movie.poster_path}`} alt="" />
+          </div>
+          <div className="col-span-8">
+            <h1 className="font-bold text-2xl lg:text-4xl mb-5">
+              {movie.title}
+            </h1>
+            {/* <p>{movie.release_date}</p> */}
+            {/* {movie.genres.map((genre) => {
             <div>{genre.name}</div>;
-          })}
-          <p>{movie.overview}</p>
+          })} */}
+            <div className="lg:w-3/4">
+              <h3 className="lg:text-2xl font-semibold mb-3">Overview</h3>
+              <p className="font-light">{movie.overview}</p>
+            </div>
+          </div>
         </div>
       </div>
 
       <div>{review.author}</div>
-    </>
+    </div>
   );
 }
